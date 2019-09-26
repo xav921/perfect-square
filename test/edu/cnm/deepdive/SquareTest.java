6fa9cdb73fca7656901e91912269df1aba1ca849
xav921 <xav7470@gmail.com>
2019-09-26 09:52:22 -0600

    anonymous class for executable file

--- test/edu/cnm/deepdive/SquareTest.java
+++ test/edu/cnm/deepdive/SquareTest.java
@@ -20,12 +20,9 @@ class SquareTest {
 
   @Test
   void isPerfectSquareException() {
-    assertThrows(IllegalArgumentException.class, new Executable() {
-      @Override
-      public void execute() throws Throwable {
-        Square.isPerfectSquare(-1);
-      }
-    });
+    assertThrows(IllegalArgumentException.class, () -> Square.isPerfectSquare(-1));
+    assertThrows(IllegalArgumentException.class, () -> Square.isPerfectSquare(-2));
+    assertThrows(IllegalArgumentException.class, () -> Square.isPerfectSquare(-3));
   }
 
 }
